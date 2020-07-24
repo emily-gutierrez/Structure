bce5b0f1269aa44b426a86d453c189a463b38716
emily-gutierrez <67969028+emily-gutierrez@users.noreply.github.com>
2020-07-24 01:39:41 -0500

    Add files via upload

---
+++ StructureCode.playground/Contents.swift
@@ -0,0 +1,26 @@
+//Windsor engine
+struct WindsorEngine {
+    var width = 24
+    var height = 27.5
+}
+let MyWindsorEngine = WindsorEngine()
+print("The width of my Windsor Engine is \(MyWindsorEngine.width)")
+print("The height of my Windsor Engine is \(MyWindsorEngine.height)")
+
+//Clevor
+struct ClevorEngine{
+    var width = 24.5
+    var height = 28.5
+}
+let MyClevorEngin = ClevorEngine()
+print("The width of my Clevor Engine is \(MyClevorEngin.width)")
+print("The height of my Clevor Engine is \(MyClevorEngin.height)")
+
+//FE
+struct FEEngine {
+    var width = 27
+    var height = 29
+}
+let MyFEEngine = FEEngine()
+print("The width of my FE Engine is\(MyClevorEngin.width)")
+print("The height of my FE Engine is\(MyClevorEngin.height)")
